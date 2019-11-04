1cf11d9f4282e547adec18b54e2c3c4dae221896
Alcides Mignoso e Silva <alcidesmig@gmail.com>
2019-11-04 03:56:50 -0300

    Adicionando script do serviço criado

---
+++ startup_launch.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+source /opt/ros/melodic/setup.bash
+source /home/pi/catkin_ws/devel/setup.bash
+sudo mount -o rw,users,umask=000 /dev/sda1 /mnt/pendrive
+roslaunch mavros px4.launch fcu_url:=/dev/serial0:921600 &
+sleep 10
+rosrun collect_data ms4525 &
+rosrun collect_data hdc1050 &
+#rosrun collect_data init &
+rosrun collect_data drone_node &

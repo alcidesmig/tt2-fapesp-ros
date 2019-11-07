302658e5a3a221f3f9b9a2a1aa708c2fd11bebb7
Alcides Mignoso <alcidesmig@gmail.com>
2019-11-06 21:58:02 -0300

    Setting up da checagem do sistema

--- startup_launch.sh
+++ startup_launch.sh
@@ -6,6 +6,6 @@ roslaunch mavros px4.launch fcu_url:=/dev/serial0:921600 &
 sleep 10
 rosrun collect_data ms4525 &
 rosrun collect_data hdc1050 &
-#rosrun collect_data init &
+rosrun collect_data init &
 rosrun collect_data drone_node2 &
 python3 /home/pi/scripts/shutdown.py &

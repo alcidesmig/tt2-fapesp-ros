54011d61ec4936aa2401cf81bd0ef12828940aa8
Alcides Mignoso <alcidesmig@gmail.com>
2019-12-07 17:51:43 -0300

    Verificando pendrive conectado

--- collect_data/src/test_system.cpp
+++ collect_data/src/test_system.cpp
@@ -17,7 +17,8 @@
 #define PIN 4
 
 mavros_msgs::State current_state;
-void state_cb(const mavros_msgs::State::ConstPtr& msg){
+void state_cb(const mavros_msgs::State::ConstPtr &msg)
+{
     current_state = *msg;
 }
 
@@ -41,7 +42,7 @@ int main(int argc, char **argv)
     ros::NodeHandle nh;
 
     ros::Subscriber state_sub = nh.subscribe<mavros_msgs::State>
-            ("mavros/state", 10, state_cb);
+                                ("mavros/state", 10, state_cb);
     // Sensor de temperatura
     ros::Subscriber hdc1050_sub = nh.subscribe<collect_data::HDC1050>
                                   ("hdc1050", 1, get_hdc1050_data);
@@ -56,53 +57,57 @@ int main(int argc, char **argv)
     wiringPiSetup();
     pinMode(PIN, OUTPUT);
 
-    pullUpDnControl(4, PUD_DOWN); 
+    pullUpDnControl(4, PUD_DOWN);
 
-    //digitalWrite(PIN, LOW);		    E
-    digitalWrite(PIN, 0); // Invertido por conta da NOT
- 
+    //digitalWrite(PIN, LOW);           E
+    digitalWrite(PIN, 1); // Invertido por conta da NOT
+
 
 
     // wait for FCU connection
-    while(ros::ok() && !current_state.connected){
+    while(ros::ok() && !current_state.connected)
+    {
         ros::spinOnce();
         rate.sleep();
 
-    	ROS_INFO("%d", current_state.connected);
+        ROS_INFO("%d", current_state.connected);
     }
-    
-    	ROS_INFO("%d", current_state.connected);
+
+    ROS_INFO("%d", current_state.connected);
 
     // Abre arquivo dos parâmetros
     FILE *fp = fopen("/home/pi/parameters.txt", "r");
 
-   
- 
+
+
 
     // Formato: [velocidade rotação (2*pi/360/VALOR_PARAMETRO)] [altitude coleta minima] [altitude coleta máxima]
     float alt_1, alt_2, alt_3, vel;
-    int return_scanf = fscanf(fp, "%f %f %f %f", &vel, &alt_1, &alt_2, &alt_3);
+    int return_scanf = fscanf(fp, "%f %f %f", &alt_1, &alt_2, &alt_3);
     fclose(fp);
+
+    fp = fopen("/mnt/pendrive/guarantee", "w");
     // Variável que indica que os parâmetros estão OK.
-    bool parameters_ok = return_scanf == 4 && (alt_1 >= 1 && alt_1 <= 100 && alt_2 >= 1 && alt_2 <= 100 && alt_3 >= 1 && alt_3 <= 100 && vel > 0 && vel <= 50);
+    bool parameters_ok = fp != NULL && return_scanf == 3 && (alt_1 >= 1 && alt_1 <= 100 && alt_2 >= 1 && alt_2 <= 100 && alt_3 >= 1 && alt_3 <= 100 && vel > 0 && vel <= 50);
+    fclose(fp);
     sleep(10);
 
     // Verifica se os dados dos sensores são válidos
     if(parameters_ok && (hdc1050.valid == 1) && (ms4525.valid == 1) && current_state.connected)
     {
         //digitalWrite(PIN, HIGH);
-	digitalWrite(PIN, 1); // Acende o LED caso tudo ok
+        digitalWrite(PIN, 0); // Acende o LED caso tudo ok
     }
     else
     {
-      //digitalWrite(PIN, LOW);
-	digitalWrite(PIN, 0); // Garantia do LED apagado
+        //digitalWrite(PIN, LOW);
+        digitalWrite(PIN, 1); // Garantia do LED apagado
     }
 
     fclose(fp);
 
 
 
-    	return 0;
+    return 0;
 }
 
